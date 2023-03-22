15d00a00d6ebcc1e5a22ac240822d8ae5a15d501
Sutee <sutee.s@linecorp.com>
2023-03-22 13:23:24 +0700

    feat: init contact section

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,4 +1,61 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
+
+interface ServiceItemProps {
+  title: string
+  detail: string
+  children?: React.ReactNode
+}
+
+const ServiceItem: React.FC<ServiceItemProps> = ({ title, detail, children }) => (
+  <div className="flex gap-2 lg:gap-6">
+    <div className='w-[100px] lg:w-[120px]'>
+      {children}
+    </div>
+    <div className='flex-1 flex flex-col gap-2'>
+      <h3 className='uppercase font-sans font-medium text-lg lg:text-xl text-black'>
+        {title}
+      </h3>
+      <p className="font-quattrocento font-normal text-sm lg:text-lg text-grey-text1">
+        {detail}
+      </p>
+    </div>
+  </div>
+);
+
+const ServiceList: React.FC = () => (
+  <div className="flex flex-col gap-8 lg:gap-16">
+    <ServiceItem
+      title="Free consultation"
+      detail="An opportunity to obtain professional advice or guidance without incurring any cost."
+    >
+      <StaticImage
+        src="../../images/services/consult.png"
+        alt="Free consultation"
+      />
+    </ServiceItem>
+
+    <ServiceItem
+      title="Production process"
+      detail="Guiding you through production processes that suit with your items or other alternative options."
+    >
+      <StaticImage
+        src="../../images/services/production-process.png"
+        alt="Production process"
+      />
+    </ServiceItem>
+
+    <ServiceItem
+      title="Follow up our team"
+      detail="Stay in touch with our team or keep up with our team's progress.."
+    >
+      <StaticImage
+        src="../../images/services/follow-up.png"
+        alt="Follow up our team"
+      />
+    </ServiceItem>
+  </div>
+);
 
 interface PureContactSectionProps {
   className?: string
@@ -18,10 +75,25 @@ const PureContactSection = React.forwardRef<HTMLElement, PureContactSectionProps
 
   return (
     <section
-      className='bg-white'
+      className='py-6 lg:py-20 px-4 lg:px-28 bg-white flex flex-col lg:flex-row gap-12 lg:gap-4 lg:justify-between'
       ref={ref}
     >
-      <div className='h-[1000px]' />
+      <div className='lg:flex-1'>
+        <h2 className='uppercase font-sans font-medium text-2.5xl lg:text-3.5xl text-black whitespace-nowrap'>
+          Get in touch
+        </h2>
+        <p className='uppercase font-sans font-medium text-sm lg:text-xl text-grey-text1 mt-1'>
+          We are here for you how can we help?
+        </p>
+
+        <div className="my-4 lg:mb-10 h-px w-15 lg:w-[120px] bg-gold"/>
+
+        <ServiceList />
+      </div>
+
+      <div className='lg:flex-1'>
+        --space for form--
+      </div>
     </section>
   );
 });
